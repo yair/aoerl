6d6b2e7b5c09d1a466ca5f355fc9199adc879387
Yair M <yairmlll@gmail.com>
2018-07-12 20:47:17 +0700

    gym environment skeleton

---
+++ gym_aoerl/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_aoerl.envs.aoerl_poloni_env import AoerlEnv
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='gym_aeorl',
+      version='0.0.1',
+      install_requires=['gym>=0.10.5']  # And any other dependencies foo needs
+)
